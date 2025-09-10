6c0069bffd1d61239dab948525cd4d5d3f76ebef
Chinemerem David Okonkwo <gentycool@yahoo.com>
2025-09-10 17:02:06 +0100

    Added new file of addtion, subtraction, and multiplication

---
+++ basic_operations.py
@@ -0,0 +1,6 @@
+number1, number2 = 10, 5
+Sum = sum(number1, number2)
+Diff = number1 - number2
+Prod = number1 * number2
+
+print(f'Addition of {number1} and {number2} is {sum(number1, number2)} \nSubtraction of {number1} and {number2} is {number1 - number2} \nMultiplication of {number1} and {number2} is {number1 * number2}')
