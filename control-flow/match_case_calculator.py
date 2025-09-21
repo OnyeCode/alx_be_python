6708b05cd735761fbf6a95d2274346064282dcad
Chinemerem Okonkwo <Engrchiok@gmail.com>
2025-09-21 03:18:13 +0100

    Done task 1

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -4,13 +4,13 @@ operation = input("Choose the operation (+, -, *, /): ").strip()
 
 match operation:
     case '+':
-        print(f" The result is {eval('num1 + num2')}")
+        print(f"The result is {eval('num1 + num2')}")
     case '-':
-        print(f" The result is {eval('num1 - num2')}")
+        print(f"The result is {eval('num1 - num2')}")
     case '*':
-        print(f" The result is {eval('num1 * num2')}")
+        print(f"The result is {eval('num1 * num2')}")
     case '/':
         if num2 == 0:
             print("Cannot divide by zero.")
         else:
-            print(f" The result is {eval('num1 // num2')}")
+            print(f"The result is {eval('num1 // num2')}")
