36fa532fd719258dae8b415ed3e8ab3a1652e50a
Chinemerem Okonkwo <Engrchiok@gmail.com>
2025-09-30 03:28:56 +0100

    edited task 0

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -9,5 +9,7 @@ def perform_operation(num1, num2, operation):
 		case 'divide':
 			if num2 == 0:
 				print("Non zero division")
-                        else:
+                        elif num2:
                                 return (num1 / num2)
+                        else:
+                            pass
